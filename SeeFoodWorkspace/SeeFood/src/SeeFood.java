fd3e7b1c4e433c0abfb598870399e9bc76e07d6f
jakeflorentine <jakeflorentine@users.noreply.github.com>
2017-10-11 21:01:18 -0400

    resizes the image upon selection
    Resizes the selected image in order to make it fit within the composites area.

--- SeeFoodWorkspace/SeeFood/src/SeeFood.java
+++ SeeFoodWorkspace/SeeFood/src/SeeFood.java
@@ -6,9 +6,11 @@ import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.graphics.Font;
 import org.eclipse.swt.graphics.GC;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
@@ -29,6 +31,57 @@ import org.eclipse.swt.widgets.Text;
  *
  */
 public class SeeFood {
+	
+	public static Image resize(Image image, Rectangle compBounds) {
+		System.out.println("Created image.  Width: " +compBounds.width + " Height: " +compBounds.height);
+		Rectangle imageBounds = image.getBounds();
+		int width = 400, height = 424;
+		//check to see whether the image is wider or taller
+		//set the corresponding dimension to the composites'
+		if(imageBounds.width > imageBounds.height) {
+			width = compBounds.width;
+			height = getRemainingBound(imageBounds, compBounds, true);
+		}
+		else {
+			height = compBounds.height;
+			width = getRemainingBound(imageBounds, compBounds, false);
+		}
+		
+		Image scaled = new Image(Display.getDefault(), width, height);
+		  GC gc = new GC(scaled);
+		  gc.setAntialias(SWT.ON);
+		  gc.setInterpolation(SWT.HIGH);
+		  gc.drawImage(image, 0, 0,image.getBounds().width, image.getBounds().height, 0, 0, width, height);
+		  gc.dispose();
+		  image.dispose();
+		  
+		  return scaled;
+		}
+	
+	/**
+	 * Calculates the remaining bound in order to display the image within the specified composite bounds.
+	 * Specify height or width with the "getHeight" value: true if the height is desired.
+	 * 
+	 * @param imageBounds Bounds of the original image
+	 * @param compBounds Bounds of the available composite space
+	 * @param getHeight True if the new height is to be returned, false otherwise.
+	 * @return The remaining bound.
+	 */
+	public static int getRemainingBound(Rectangle imageBounds, Rectangle compBounds, boolean getHeight) {
+		int remainingBound = 0;
+		double pct;
+		
+		if(getHeight) {
+			pct = (double)compBounds.width/(double)imageBounds.width;
+			remainingBound = (int)(pct*imageBounds.height);
+		}
+		else {
+			pct = (double)compBounds.height/(double)imageBounds.height;
+			remainingBound = (int)(pct*imageBounds.width);
+		}
+		
+		return remainingBound;		
+	}
 
 	/**
 	 * @param args
@@ -57,6 +110,7 @@ public class SeeFood {
         Label t = new Label(c, SWT.NONE);
         t.setLayoutData(new GridData(SWT.CENTER, SWT.FILL, true, false, 3, 1));
         t.setText("SeeFood");
+        t.setFont( new Font(display,"Arial", 28, SWT.BOLD ) );
         
         //Composite should be a "drop target"
         
@@ -84,11 +138,10 @@ public class SeeFood {
 					String si = parentPath+fd.getFileName();
 					System.out.println(si);
 					Image i = new Image(display, parentPath+"/"+fd.getFileName());
-					
-					
-					System.out.println("Created image");
-					ci.setBackground(new Color(display, 0, 255, 0));
-					ci.setBackgroundImage(i);
+					Image scaled = resize(i, ci.getBounds());
+				
+					//set the composite background to be the scaled image
+					ci.setBackgroundImage(scaled);
 				}
 				catch(Exception e1) {
 					System.out.println("Cannot create image");
