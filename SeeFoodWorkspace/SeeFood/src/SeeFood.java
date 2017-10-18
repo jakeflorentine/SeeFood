8d38e6d0af92c49defd5d99ac03480fde453bfae
jakeflorentine <jakeflorentine@users.noreply.github.com>
2017-10-17 21:58:13 -0400

    Updated View Package
    Updates to the view package and removed warnings.

--- SeeFoodWorkspace/SeeFood/src/SeeFood.java
+++ SeeFoodWorkspace/SeeFood/src/SeeFood.java
@@ -1,27 +1,3 @@
-import java.io.File;
-
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.custom.CLabel;
-import org.eclipse.swt.events.SelectionAdapter;
-import org.eclipse.swt.events.SelectionEvent;
-import org.eclipse.swt.events.SelectionListener;
-import org.eclipse.swt.graphics.Color;
-import org.eclipse.swt.graphics.Font;
-import org.eclipse.swt.graphics.GC;
-import org.eclipse.swt.graphics.Image;
-import org.eclipse.swt.graphics.Point;
-import org.eclipse.swt.graphics.Rectangle;
-import org.eclipse.swt.layout.FillLayout;
-import org.eclipse.swt.layout.GridData;
-import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.widgets.Button;
-import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Display;
-import org.eclipse.swt.widgets.FileDialog;
-import org.eclipse.swt.widgets.Label;
-import org.eclipse.swt.widgets.Shell;
-import org.eclipse.swt.widgets.Text;
-
 import view.SeefoodView;
 
 /**
@@ -38,7 +14,7 @@ public class SeeFood {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		SeefoodView.createContent();	
+		SeefoodView.createContent();
 	}
 
 }
