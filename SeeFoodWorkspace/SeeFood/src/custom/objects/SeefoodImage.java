4b08d322188cf8e7c8591af43c24e42fa1577c15
jakeflorentine <jakeflorentine@users.noreply.github.com>
2017-10-22 21:12:19 -0400

    Added all packages to the project
    custom.objects and custom.widgets have been added as packages to the system

---
+++ SeeFoodWorkspace/SeeFood/src/custom/objects/SeefoodImage.java
@@ -0,0 +1,8 @@
+package custom.objects;
+
+import org.eclipse.swt.graphics.Image;
+
+public class SeefoodImage {
+	private int confidenceLevel = 0;
+	private Image originalImage;
+}
---
+++ SeeFoodWorkspace/SeeFood/src/custom/widgets/ImageComposite.java
@@ -0,0 +1,8 @@
+package custom.widgets;
+
+import custom.objects.SeefoodImage;
+
+public class ImageComposite {
+	private SeefoodImage seefoodImage;
+	private ConfidenceMeter confidenceMeter;
+}
