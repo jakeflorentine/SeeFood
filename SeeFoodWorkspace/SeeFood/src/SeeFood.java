662b6df821170b3b6b00a26a80f81225b741312d
jakeflorentine <jakeflorentine@users.noreply.github.com>
2017-10-25 08:45:41 -0400

    Removed Jars
    removed swt jars which caused runtime issues on other machines

--- SeeFoodWorkspace/SeeFood/src/SeeFood.java
+++ SeeFoodWorkspace/SeeFood/src/SeeFood.java
@@ -10,15 +10,7 @@ import view.SeefoodView;
  */
 public class SeeFood {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		loadSwtJar();
-		SeefoodView.createContent();
-	}
-
-	private static void loadSwtJar() {
+	private void loadSwtJar() {
 		String osName = System.getProperty("os.name").toLowerCase();
 		String osArch = System.getProperty("os.arch").toLowerCase();
 		String swtFileNameOsPart = osName.contains("win") ? "win32"
@@ -38,14 +30,23 @@ public class SeeFood {
 		// URL.class);
 		// addUrlMethod.setAccessible(true);
 		//
-		// URL swtFileUrl = new URL("rsrc:"+swtFileName); // I am using Jar-in-Jar class
-		// loader which understands this URL; adjust accordingly if you don't
+		// URL swtFileUrl = new URL("rsrc:" + swtFileName); // I am using Jar-in-Jar
+		// class
+		// // loader which understands this URL; adjust accordingly if you don't
 		// addUrlMethod.invoke(classLoader, swtFileUrl);
+		// } catch (Exception e) {
+		// throw new RuntimeException("Unable to add the SWT jar to the class path:" +
+		// swtFileName, e);
 		// }
-		// catch(Exception e) {
-		// throw new RuntimeException("Unable to add the SWT jar to the class path:
-		// "+swtFileName, e);
-		// }
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		SeeFood s = new SeeFood();
+		s.loadSwtJar();
+		SeefoodView.createContent();
 	}
 
 }
