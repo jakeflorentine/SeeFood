27b5d9e019c90552fb1ddc81b3a5db9a7954bbde
Matt Anderson <anderson.302@wright.edu>
2017-10-26 12:42:54 -0400

    Created the custom image object (SeefoodImage)
    Constructor, getters, setters, and variable types are set.

--- SeeFoodWorkspace/SeeFood/src/custom/objects/SeefoodImage.java
+++ SeeFoodWorkspace/SeeFood/src/custom/objects/SeefoodImage.java
@@ -3,7 +3,27 @@ package custom.objects;
 import org.eclipse.swt.graphics.Image;
 
 public class SeefoodImage {
-	// TBD
-	private int confidenceLevel = 0;
+	private double confidenceLevel = 0;
 	private Image originalImage;
+
+	public SeefoodImage(double confidence, Image image) {
+		this.confidenceLevel = confidence;
+		this.originalImage = image;
+	}
+
+	public void SetConfidence(double confidence) {
+		this.confidenceLevel = confidence;
+	}
+
+	public double GetConfidence() {
+		return this.confidenceLevel;
+	}
+
+	public void SetImage(Image image) {
+		this.originalImage = image;
+	}
+
+	public Image GetImage() {
+		return this.originalImage;
+	}
 }
