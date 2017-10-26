7521c0b76510d2a6179842670604eebd06bf8157
Matt <thedamnstein@gmail.com>
2017-10-26 15:58:55 -0400

    Create ParserUtil.java
    Stupid github desktop won't acknowledge that I added this in eclipse

---
+++ SeeFoodWorkspace/SeeFood/src/util/ParserUtil.java
@@ -0,0 +1,15 @@
+package util;
+
+import java.util.ArrayList;
+
+public class ParserUtil {
+
+	public static String[] ParseFiles(String[] filenames) {
+		ArrayList<String> invalidFiles = new ArrayList<String>();
+
+		for (String filename : filenames) {
+
+		}
+	}
+
+}
