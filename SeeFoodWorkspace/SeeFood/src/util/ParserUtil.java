14dd802933af2018a08ca9ad7a341f9bb35e1f66
Matt Anderson <anderson.302@wright.edu>
2017-10-29 17:57:38 -0400

    Only accepting JPEG, JPG, BMP, PNG

--- SeeFoodWorkspace/SeeFood/src/util/ParserUtil.java
+++ SeeFoodWorkspace/SeeFood/src/util/ParserUtil.java
@@ -1,6 +1,8 @@
 package util;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.widgets.Display;
@@ -23,14 +25,17 @@ public class ParserUtil {
 	}
 
 	private static boolean isValid(String filename) {
-		try {
-			Image image = new Image(Display.getCurrent(), filename);
-			// SWT was able to use this file as an image
-			return true;
-		} catch (Exception e) {
-			// Not a valid file type or not a valid image type
-			return false;
-		}
+		List<String> filetypes = new ArrayList<>(Arrays.asList("xyz", "abc"));
+		String extension = filename.substring(filename.lastIndexOf('.'));
+		return filetypes.contains(extension.toLowerCase());
+//		try {
+//			Image image = new Image(Display.getCurrent(), filename);
+//			// SWT was able to use this file as an image
+//			return true;
+//		} catch (Exception e) {
+//			// Not a valid file type or not a valid image type
+//			return false;
+//		}
 	}
 
 }
