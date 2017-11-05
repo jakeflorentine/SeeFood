d791eb5ae57128d8e833c06be234859869b02839
jakeflorentine <jakeflorentine@users.noreply.github.com>
2017-11-05 13:20:52 -0500

    Updates to the HomeView

--- SeeFoodWorkspace/SeeFood/src/view/HomeView.java
+++ SeeFoodWorkspace/SeeFood/src/view/HomeView.java
@@ -24,13 +24,13 @@ public class HomeView extends Composite {
 	}
 
 	public void createContent(Composite parent, int style) {
+		Composite btnComp = new Composite(parent, SWT.BORDER | SWT.NO_BACKGROUND);
+		btnComp.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, true, 3, 1));
+		btnComp.setLayout(new GridLayout(2, true));
 
-		Composite ci = new Composite(parent, SWT.BORDER | SWT.NO_BACKGROUND);
-		ci.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 3, 3));
-
-		Button upload = new Button(parent, SWT.FLAT);
+		Button upload = new Button(btnComp, SWT.FLAT);
 		upload.setText("Upload");
-		upload.setLayoutData(new GridData(SWT.CENTER, SWT.CENTER, true, false, 3, 1));
+		upload.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, true, false, 1, 1));
 		upload.addSelectionListener(new SelectionAdapter() {
 
 			@Override
@@ -46,10 +46,10 @@ public class HomeView extends Composite {
 				fd.setText("Select a photo");
 				fd.open();
 				String[] files = fd.getFileNames();
-				
+
 				files = ParserUtil.parseFiles(files);
-				
-				if(files.length > 0) {
+
+				if (files.length > 0) {
 					String parentFilePath = fd.getFilterPath();
 					openUploadView(parentFilePath, files);
 					// b.setBackgroundImage(new Image(display, parentPath+fd.getFileName()));
@@ -64,9 +64,9 @@ public class HomeView extends Composite {
 
 		});
 
-		Button gallery = new Button(parent, SWT.FLAT);
+		Button gallery = new Button(btnComp, SWT.FLAT);
 		gallery.setText("Gallery");
-		gallery.setLayoutData(new GridData(SWT.CENTER, SWT.CENTER, true, false, 3, 1));
+		gallery.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, true, false, 1, 1));
 		gallery.addSelectionListener(new SelectionAdapter() {
 
 			@Override
